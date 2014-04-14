c654916d2236a17c8c2663d2982771bf17070dc0
swqqn <mail2hack@yandex.ru>
2014-04-14 11:42:59 +0000

    Don't hardcode lockname in test.

--- post_office/logutils.py
+++ post_office/logutils.py
@@ -15,7 +15,7 @@ def setup_loghandlers(level=None):
             "formatters": {
                 "post_office": {
                     "format": "[%(levelname)s]%(asctime)s PID %(process)d: %(message)s",
-                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                    "datefmt": "%d-%m-%Y %H:%M:%S",
                 },
             },
 
