f52150036c617cf0502f2dea754535db8877cc9a
swqqn <mail2hack@yandex.ru>
2014-04-14 11:18:06 +0000

    Use a more Y-m-d date format in logutils.

--- post_office/tests/lockfile.py
+++ post_office/tests/lockfile.py
@@ -6,70 +6,43 @@ from django.test import TestCase
 from ..lockfile import FileLock, FileLocked
 
 
-def setup_fake_lock(lock_file_name):
-    pid = os.getpid()
-    lockfile = '%s.lock' % pid
-    try:
-        os.remove('test.lock')
-    except OSError:
-        pass
-    os.symlink(lockfile, 'test.lock')
-
-
 class LockTest(TestCase):
 
     def test_process_killed_force_unlock(self):
-        pid = os.getpid()
-        lockfile = '%s.lock' % pid
-        setup_fake_lock('test.lock')
-
+        lockfile = 'test.txt.lock'
         with open(lockfile, 'w+') as f:
             f.write('9999999')
         assert os.path.exists(lockfile)
-        with FileLock('test'):
+        with FileLock('test.txt'):
             assert True
 
     def test_force_unlock_in_same_process(self):
-        pid = os.getpid()
-        lockfile = '%s.lock' % pid
-        os.symlink(lockfile, 'test.lock')
-
+        lockfile = 'test.txt.lock'
         with open(lockfile, 'w+') as f:
             f.write(str(os.getpid()))
 
-        with FileLock('test', force=True):
+        with FileLock('test.txt', force=True):
             assert True
 
     def test_exception_after_timeout(self):
-        pid = os.getpid()
-        lockfile = '%s.lock' % pid
-        setup_fake_lock('test.lock')
-
+        lockfile = 'test.txt.lock'
         with open(lockfile, 'w+') as f:
             f.write(str(os.getpid()))
 
         try:
-            with FileLock('test', timeout=1):
+            with FileLock('test.txt', timeout=1):
                 assert False
         except FileLocked:
             assert True
 
     def test_force_after_timeout(self):
-        pid = os.getpid()
-        lockfile = '%s.lock' % pid
-        setup_fake_lock('test.lock')
-
+        lockfile = 'test.txt.lock'
         with open(lockfile, 'w+') as f:
             f.write(str(os.getpid()))
 
         timeout = 1
         start = time.time()
-        with FileLock('test', timeout=timeout, force=True):
+        with FileLock('test.txt', timeout=timeout, force=True):
             assert True
         end = time.time()
         assert end - start > timeout
-
-    def test_get_lock_pid(self):
-        """Ensure get_lock_pid() works properly"""
-        with FileLock('test', timeout=1, force=True) as lock:
-            self.assertEqual(lock.get_lock_pid(), int(os.getpid()))
