7b5cf37542d49a3919d45f3b99929177a53d197c
swqqn <mail2hack@yandex.ru>
2015-06-04 04:05:55 +0000

    Fixes importlib deprecation warning in Django 1.8.

--- post_office/admin.py
+++ post_office/admin.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.db import models
 from django.contrib import admin
 from django.forms.widgets import TextInput
 from django.utils import six
 from django.utils.text import Truncator
 
 from .fields import CommaSeparatedEmailField
-from .models import Email, Log, EmailTemplate, TranslatedEmailTemplate, STATUS
+from .models import Email, Log, EmailTemplate, STATUS
 
 
 def get_message_preview(instance):
@@ -67,21 +64,6 @@ class LogAdmin(admin.ModelAdmin):
     list_display = ('date', 'email', 'status', get_message_preview)
 
 
-class SubjectField(TextInput):
-    def __init__(self, *args, **kwargs):
-        super(SubjectField, self).__init__(*args, **kwargs)
-        self.attrs.update({'style': 'width: 610px;'})
-
-
-class TranslatedEmailTemplateInline(admin.StackedInline):
-    model = TranslatedEmailTemplate
-    extra = 0
-    fields = ('language', 'subject', 'content', 'html_content',)
-    formfield_overrides = {
-        models.CharField: {'widget': SubjectField}
-    }
-
-
 class EmailTemplateAdmin(admin.ModelAdmin):
     list_display = ('name', 'description_shortened', 'subject', 'created')
     search_fields = ('name', 'description', 'subject')
@@ -93,10 +75,6 @@ class EmailTemplateAdmin(admin.ModelAdmin):
             'fields': ('subject', 'content', 'html_content'),
         }),
     ]
-    inlines = (TranslatedEmailTemplateInline,)
-    formfield_overrides = {
-        models.CharField: {'widget': SubjectField}
-    }
 
     def description_shortened(self, instance):
         return Truncator(instance.description.split('\n')[0]).chars(200)
