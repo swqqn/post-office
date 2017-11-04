9be269df68e56d6f8aa2ce84ddc61a465b37bc00
swqqn <mail2hack@yandex.ru>
2017-11-04 01:42:44 +0000

    Added translation for BCC

--- post_office/apps.py
+++ post_office/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
 
 
 class PostOfficeConfig(AppConfig):
     name = 'post_office'
-    verbose_name = _("Post Office")
+    verbose_name = "Post Office"
